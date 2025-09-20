147d21931711e48794fd15955f9d0eb3856749e2
Taran-Sandhu <233038265+Taran-Sandhu@users.noreply.github.com>
2025-09-20 10:42:59 +1000

    Clean up

--- server.py
+++ server.py
@@ -110,5 +110,5 @@ if __name__ == "__main__":
     import uvicorn
     import os
 
-    port = int(os.environ.get("PORT", 5001))  # default 5001, override with $PORT
+    port = int(os.environ.get("PORT", 5001))
     uvicorn.run(app, host="0.0.0.0", port=port)
